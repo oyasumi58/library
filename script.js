16582b3896b66fe3468dbd69ace1596474675321
oyasumi58 <190088115+oyasumi58@users.noreply.github.com>
2025-04-24 19:06:34 +0800

    Attach js to DOM

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 const myLibrary = [];
 
-function Book(name,author,read) {
+function Book(title,author,pageno,read) {
     this.id = crypto.randomUUID();
-    this.name = name;
+    this.title = title;
     this.author = author;
+    this.pageno = pageno;
     this.read = read;
 }
 
-function addBookToLibrary(name,author,read) {
-    let book = new Book(name,author,read);
+let book;
+
+function addBookToLibrary(title,author,pageno,read) {
+    book = new Book(title,author,pageno,read);
     myLibrary.push(book);
-    return `${book.name} has been placed in the library!`;
+    createCard(book);
+    return `${book.title} has been placed in the library!`;
 }
 
-console.log(addBookToLibrary('The hobbit','JRR tolkien',false));
+//test add 3 books
+addBookToLibrary('The hobbit','JRR tolkien',65,false)
+addBookToLibrary('The cat','FRR Feline',650,false)
+addBookToLibrary('The dog','WRR Wolf',6500,false)
+
+//DOM stuff
+function createCard(book) {
+    let bookObj = book;
+    console.log(bookObj.author);
+    let gridCon = document.querySelector(".container");
+    let cardNode = document.createElement("div");
+    cardNode.setAttribute("class","card");
+    gridCon.appendChild(cardNode);
+    
+    let bookTitle = document.createElement("p");
+    bookTitle.textContent = book.title;
+
+    let bookAuthor = document.createElement("p");
+    bookAuthor.textContent = `${book.author}`;
+    let bookPageNo = document.createElement("p");
+    bookPageNo.textContent = `${book.pageno}`;
+    //console.log(bookAuthor);
+
+    cardNode.appendChild(bookTitle);
+    cardNode.appendChild(bookAuthor);
+    cardNode.appendChild(bookPageNo);
+}
+
